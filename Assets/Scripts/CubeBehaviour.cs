d13a8d118a96c7f87f1333fd6e463499224e46b9
wrapping-2000 <wenpengchen@njust.edu.cn>
2023-08-09 20:27:08 +0800

    feat: Make a Unity 3D Mobile games app example for Casdoor OAuth/OIDC/SSO.

---
+++ Assets/Scripts/CubeBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CubeBehaviour : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float jumpForce = 10f;
+    public float time = 1f;
+    public bool loseGame;
+    [SerializeField]
+    private LineRenderer line;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!loseGame)
+        {
+            if (Input.GetMouseButton(0) && Mathf.Approximately(rb.velocity.x, 0) && Mathf.Approximately(rb.velocity.y, 0))
+            {
+                line.positionCount = (int)(time * 10);
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15));
+                line.SetPosition(0, transform.position);
+                Vector3 startVelocity = (transform.position - mousePosition) * jumpForce;
+                if (startVelocity.x < 0 && startVelocity.y > 0 && startVelocity.z > 0)
+                {
+                    PlotTrajectory(transform.position, startVelocity, time / 10, time);
+                }
+                else
+                {
+                    line.positionCount = 0;
+                }
+            }
+            if (Input.GetMouseButtonUp(0) && line.positionCount > 0)
+            {
+                line.positionCount = 0;
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15));
+                rb.AddForce((transform.position - mousePosition) * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    public void PlotTrajectory(Vector3 start, Vector3 startVelocity, float timestep, float maxTime)
+    {
+        Vector3 prev = start;
+        for (int i = 0; i < line.positionCount; i++)
+        {   
+            float t = timestep * i;
+            if (t > maxTime) break;
+            Vector3 pos = PlotTrajectoryAtTime(start, startVelocity, t);
+            if (Physics.Linecast(prev, pos)) break;
+            line.SetPosition(i, pos);
+            prev = pos;
+        }
+    }
+
+    public Vector3 PlotTrajectoryAtTime(Vector3 start, Vector3 startVelocity, float time)
+    {
+        return start + startVelocity * time + Physics.gravity * time * time * 0.5f;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # casdoor-unity-example
+
+The example uses the [Casdoor demo site](https://door.casdoor.com/) server and [`Casdoor.Client`](https://github.com/casdoor/casdoor-dotnet-sdk/tree/master/src/Casdoor.Client) SDK for `.NET` in [casdoor-dotnet-sdk](https://github.com/casdoor/casdoor-dotnet-sdk/). 
+
+The game in this example is based on [ValleyOfCubes_Unity3D](https://github.com/oussamabonnor1/ValleyOfCubes_Unity3D).
+
+## Quick Start
+
+- download the code
+
+```bash
+ git clone git@github.com:Wrapping-2000/casdoor-unity-example.git
+```
+
+- Open the newly downloaded code in `Unity Hub` and run it.
+
+## After running, you will see the following  interfaces:
+
+|                           **iOS**                           |                         **Android**                          |
+| :---------------------------------------------------------: | :----------------------------------------------------------: |
+| <img src="./iOS-gif.gif" alt="iOS-gif" style="zoom:50%;" /> | <img src="./Android-gif.gif" alt="Android-gif" style="zoom: 50%;" /> |
+
+
+
+## Configure
+
+Initialization requires 6 parameters, which are all str type:
+| Name (in order)  | Must | Description                                            |
+| ---------------- | ---- | ------------------------------------------------------ |
+| Endpoint         | Yes  | Casdoor Server Url, such as `https://door.casdoor.com` |
+| OrganizationName | Yes  | Organization name                                      |
+| ApplicationName  | Yes  | Application name                                       |
+| ApplicationType  | Yes  | webapp, webapi or native                               |
+| ClientId         | Yes  | Your client id                                         |
+| ClientSecret     | Yes  | Your client secret                                     |
+
+```C#
+var httpClient = new HttpClient();
+var client = new CasdoorClient(HttpClient, new CasdoorOptions{
+    Endpoint = "https://door.casdoor.com",
+    OrganizationName = "build-in",
+    ApplicationName = "app-build-in",
+    ApplicationType = "native", // webapp, webapi or native
+    ClientId = "<your client id>",
+    ClientSecret = "<your client secret>",
+});
+```
+
+## License
+
+This project is licensed under the [Apache 2.0 license](https://github.com/casdoor/casdoor-dotnet-sdk/blob/master/LICENSE).

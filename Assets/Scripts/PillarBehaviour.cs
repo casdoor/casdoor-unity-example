1371968ec6fea8109b53c847bf6225129f029521
wrapping-2000 <wenpengchen@njust.edu.cn>
2023-08-13 20:10:50 +0800

    feat: add  ValleyOfCubes_Unity3D game.

---
+++ Assets/Scripts/PillarBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarBehaviour : MonoBehaviour
+{
+
+    public GameManager gameManager;
+    public GameObject cube;
+    public int score = 0;
+    public bool isLast = false;
+    bool visited = false;
+
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Cube"))
+        {
+            if (!visited)
+            {
+                visited = true;
+                gameManager.scoreUp(score);
+                if (isLast)
+                {
+                    StartCoroutine(gameManager.nextLevelPanel());
+                }
+            }
+        }
+    }
+
+
+}
